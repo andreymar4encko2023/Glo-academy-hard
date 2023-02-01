3dc9a3e05afb2f7f625efaa6b8b7da2f62dd814e
andreymar4encko2023 <123625074+andreymar4encko2023@users.noreply.github.com>
2023-02-01 03:40:15 +0300

    Add files via upload

---
+++ lessond3hard/main.js
@@ -0,0 +1,43 @@
+let langbtn = document.querySelectorAll('.btn')
+
+
+langbtn.forEach(item  => {
+
+   item.addEventListener('click',()=>{
+
+    lang =item.innerText;
+    
+    if(lang ==="ru"|| lang ==='Ru' || lang ==='RU' || lang ==='rU') {
+
+        console.log('понедельник вторник среда четерг пятница субота воскресенье');
+
+    } else if (lang === 'en' || lang==='En'|| lang === 'EN' || lang ==='En') {
+
+        console.log('Monday  Tuesday  Wednesday  Thursday  Friday  Saturday  Sunday');
+    }
+
+   })
+   
+});
+
+                           //  второй вариант
+// lang = prompt('выберите язык ru или en');
+
+//  if(lang === "ru"|| lang === 'Ru' || lang === 'RU' || lang === 'rU'){
+
+//     console.log('понедельник вторник среда четерг пятница субота воскресенье');
+
+//    } else if (lang === 'en' || lang ==='En'|| lang === 'EN' || lang ==='En'); {
+
+//     console.log ('Monday  Tuesday  Wednesday  Thursday  Friday  Saturday  Sunday');    
+
+// } else {
+
+//     console.log('неправильно указан язык попробуйте еще')
+    
+   
+   
+// }
+const namePerson = 'Александр';
+
+namePerson == 'Артем' ? console.log('Директор') : namePerson=='Александр' ? console.log('преподаватель') : console.log('студент');
